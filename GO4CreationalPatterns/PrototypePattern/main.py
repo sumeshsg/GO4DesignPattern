30fb5085819e511584b62f7f574bf1ce57ff22dc
sumeshsg@gmail.com <sumeshsg@nbnco.com.au>
2020-03-30 17:31:17 +1100

    Updat the README file

--- GO4CreationalPatterns/PrototypePattern/main.py
+++ GO4CreationalPatterns/PrototypePattern/main.py
@@ -5,8 +5,8 @@ if __name__ == '__main__':
     developer_ushas = developer_sg.clone()
     developer_ushas.set_name('Ushas')
 
-    print ("Developer SG Details")
-    print (developer_sg.get_name(),developer_sg.get_skill())
+    print("Developer SG Details")
+    print(developer_sg.get_name(), developer_sg.get_skill())
 
     print("Developer Ushas Details")
     print(developer_ushas.get_name(), developer_ushas.get_skill())
